41f660b65d4cd6a788d77d10a7709fa31678b1aa
Shrinidhi Pandit <shrinidhipandit02@gmail.com>
2025-02-22 12:04:33 +0530

    Update ASSIGNMENT2 SQL.sql

--- Queries/ASSIGNMENT2 SQL.sql
+++ Queries/ASSIGNMENT2 SQL.sql
@@ -49,5 +49,24 @@ WHERE
     ((POPULATION BETWEEN 100000000 AND 200000000)
         OR (AREA_KM2 > 5000000))
         AND (NAME NOT LIKE '% %'); 
+
+set sql_safe_updates=0;
+
+SELECT * FROM HR.SALES_PEOPLE;
+
+/*
+1. From the ORDER_ITEMS table in the CUSTOMER_ORDERS schema return a column called line_item_amount 
+that is the unit_price multiplied by the quantity for each record. (Return all other columns as well)
+
+2. Round the line_item_amount column to 1 decimal point.
+*/
+
+USE CUSTOMER_ORDERS;
+
+SELECT ORDER_ID,PRODUCT_ID, ROUND(UNIT_PRICE * QUANTITY,1) AS LINE_ITEM_AMOUNT FROM CUSTOMER_ORDERS.ORDER_ITEMS;
+
+
+
+
         
 -- 
