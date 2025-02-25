ab60cb7e18c05cf965d7b3d2565fc075413ffe75
Shrinidhi444 <shrinidhipandit02@gmail.com>
2025-02-25 11:10:29 +0530

    up

--- Queries/ASSIGNMENT2 SQL.sql
+++ Queries/ASSIGNMENT2 SQL.sql
@@ -49,6 +49,42 @@ WHERE
     ((POPULATION BETWEEN 100000000 AND 200000000)
         OR (AREA_KM2 > 5000000))
         AND (NAME NOT LIKE '% %'); 
+        
+-- NEW TABLE
+
+/*
+1. Create a table in the HR schema called SALES_PEOPLE. The structure of this table should be the same as the EMP table... with one difference:
+
+==> Instead of the SAL and COMM columns hava a column called TOTAL_PAY
+*/
+/* 
+2. Insert records into the SALES_PEOPLE table from the EMP table with only the records where the job is SALESMAN. (SAL + COMM = TOTAL_PAY)
+*/
+
+
+USE HR;
+
+CREATE TABLE HR.SALES_PEOPLE AS SELECT EMP_NO,
+    E_NAME,
+    JOB,
+    MGR,
+    HIRE_DATE,
+    (SAL + COMM) AS TOTAL_PAY,
+    DEPT_NO FROM
+    HR.EMP
+WHERE
+    jOB = 'SALESMAN';
+;
+
+SELECT * FROM HR.SALES_PEOPLE;
+/*
+update the records to change the job title from "SALESMAN" TO "SENIOR SALESMAN" if the employee TOTAL_PAY is over 2000.
+*/
+
+UPDATE HR.SALES_PEOPLE
+SET JOB='SENIOR_SALESMAN'
+WHERE TOTAL_PAY > 2000;
+;
 
 set sql_safe_updates=0;
 
@@ -65,8 +101,38 @@ USE CUSTOMER_ORDERS;
 
 SELECT ORDER_ID,PRODUCT_ID, ROUND(UNIT_PRICE * QUANTITY,1) AS LINE_ITEM_AMOUNT FROM CUSTOMER_ORDERS.ORDER_ITEMS;
 
+/* jupiter commit*/
 
+select * from booklist.bookdetail;
 
+/* practice assignment*/
 
-        
--- 
+use hr;
+
+select * from hr.sales_people
+where TOTAL_PAY > 10000;
+
+select 
+min(total_pay) as min_t,
+max(total_pay) as max_t,
+avg(total_pay) as avg_t
+from sales_people;
+
+select * from hr.sales_people
+order by TOTAL_PAY desc
+limit 3;
+
+/*
+select sp.ename,sp.total_pay as mname,mpay
+from sales_people sp
+join sales_people m on sp.mrg = m.empno
+where sp.total_pay > m.total_pay;
+*/
+
+-- percentage of total pay
+SELECT 
+    E_NAME, 
+    TOTAL_PAY, 
+    (TOTAL_PAY / (SELECT SUM(TOTAL_PAY) FROM SALES_PEOPLE) * 100) AS Percentage_Contribution
+FROM HR.SALES_PEOPLE;
+
